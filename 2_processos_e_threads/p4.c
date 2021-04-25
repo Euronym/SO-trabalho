b75c56f76313439a69af5e08a14fd5a0faff18be
Euronym <brunos-martins@hotmail.com>
2021-04-25 18:09:23 -0300

    modificações possivelmente finais das sincronizações

--- 2_processos_e_threads/p4.c
+++ 2_processos_e_threads/p4.c
@@ -10,56 +10,67 @@
 	adicionem -lpthread ao final para compilar.
 	ex: gcc p4.c -o p4 -lpthread
 */
-sem_t condA, condB; // cria uma variável para sincronizar as threads.
+sem_t condA, condB; // cria semáforos para sincronizar as threads.
 pthread_mutex_t o_mutex; // cria uma variável mutex para controlar o acesso.
-int cont = 0;
+int cont = 0; // cria uma variável cont para servir de flag
+
 void* printA()
 {
+
 	pthread_mutex_lock(&o_mutex); // requisita o acesso.
-	if(cont != 0)
-    	{		
-        	pthread_mutex_unlock(&o_mutex);
-        	sched_yield();
-    	}
-    	else
-    	{
-		 printf("A\n");
-		 cont++;
-		 sem_post(&condA); // emite um comando para o thread B.
-		 pthread_mutex_unlock(&o_mutex); // libera o acesso.
-		 pthread_exit(0); //para finalizar a thread
-   	}	
+	printf("A\n");
+	// modifica cont para 2, sinalizando que b pode executar.
+	cont = 2;
+	sem_post(&condA); // emite um comando para o thread B.
+	pthread_mutex_unlock(&o_mutex); // libera o acesso.
+	pthread_exit(0); //para finalizar a thread
 }
 
 void* printB()
 {
-	pthread_mutex_trylock(&o_mutex); // tenta utilizar o recurso.
-	sem_wait(&condA); // aguarda o sinal de A.
-	cont = 0;
-	printf("B\n");
-	sem_post(&condB); // emite um comando para o thread C.
-	pthread_mutex_unlock(&o_mutex); // libera o recurso.
-	pthread_exit(0);
 
+	while(1)
+	{
+		pthread_mutex_trylock(&o_mutex); // tenta utilizar o recurso.
+		// se b ainda não estiver na vez para ser executado, cede a vez.
+		if(cont != 2)
+		{
+
+			pthread_mutex_unlock(&o_mutex); // libera o acesso ao recurso.
+			sched_yield(); // passa a vez.
+		}
+		else
+		{
+			sem_wait(&condA); // aguarda o sinal de A.
+			cont = 3;
+			printf("B\n");
+			sem_post(&condB); // emite um comando para o thread C.
+			pthread_mutex_unlock(&o_mutex); // libera o recurso.
+			break;
+		}
+	}
+	pthread_exit(0);
 }
 
 void* printC()
 {
+
 	while(1)
 	{
-	pthread_mutex_trylock(&o_mutex); // tenta utilizar o recurso.
-	if(cont != 0)
-	{
-	    sched_yield();
-	    pthread_mutex_unlock(&o_mutex);
-	}
-	else
-	{
-	    sem_wait(&condB);// aguarda o sinal de B.
-		printf("C\n");
-		pthread_mutex_unlock(&o_mutex); // libera o acesso.
-		break;
-	}
+		pthread_mutex_trylock(&o_mutex); // tenta utilizar o recurso.
+		// se c ainda não estiver na vez, cede a vez.
+		if(cont != 3)
+		{
+	    	sched_yield();
+	    	pthread_mutex_unlock(&o_mutex);
+		}
+		else
+		{
+	   		sem_wait(&condB);// aguarda o sinal de B.
+			printf("C\n");
+			pthread_mutex_unlock(&o_mutex); // libera o acesso.
+			break;
+		}
 	}
 	pthread_exit(0);
 }
@@ -70,20 +81,20 @@ int main()
 	pthread_t tA, tB, tC;
 
 	pthread_mutex_init(&o_mutex,0); // inicializa o mutex.
-    	sem_init(&condA, 0, 0);
-    	sem_init(&condB, 0, 0);
+    sem_init(&condA, 0, 0);
+    sem_init(&condB, 0, 0);
 
    	pthread_create(&tA,0,printA,0);
    	pthread_create(&tB,0,printB,0);
    	pthread_create(&tC,0,printC,0);
 
-    	pthread_join(tA, 0); //espera execução da thread com id = tA (que imprime A)
+    pthread_join(tA, 0); //espera execução da thread com id = tA (que imprime A)
 	pthread_join(tB, 0); // espera a execução da thread com id = TB
 	pthread_join(tC, 0); // espera a execução da thread com id = TC
 
 	sem_destroy(&condA); // destroi a variavel de condicao A.
-    	sem_destroy(&condB); // destroi a variavel de condicao B.
-    	pthread_mutex_destroy(&o_mutex); // destroi o mutex.
+    sem_destroy(&condB); // destroi a variavel de condicao B.
+    pthread_mutex_destroy(&o_mutex); // destroi o mutex.
 
     	return 0;
 }
--- 2_processos_e_threads/p6_threads.c
+++ 2_processos_e_threads/p6_threads.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <sched.h>
 #include <semaphore.h>
 
 // Quantidade de números no array //
@@ -18,14 +19,19 @@ pthread_mutex_t mutex;
 // Condição, utilizando semáforo, para a sincronização das threads //
 sem_t sort_condition;
 
+// cria uma flag para a sincronização das threads.
+int cont = 0;
+
 int numeros[BUFFER] = {9, 3, 2, 5, 1, 0, 4, 7, 6}; // Arrays de inteiros para ordenação //
 
-int main() {
+int main() 
+{
     // inicia a contagem dos ciclos de clock necessários para o processo.
     clock_t begin = clock();
 
 	pthread_t sort_thread, printer_thread;
 	pthread_mutex_init(&mutex, 0);
+	sem_init(&sort_condition, 0 ,0);
 	printf("\x1b[31mCriando uma thread para ordenação.\x1b[0m\n");
 
 	pthread_create(&sort_thread,0, bubble_sort,0); // Cria uma thread para ordenação //
@@ -45,30 +51,51 @@ int main() {
 	return 0;
 }
 
-void *bubble_sort() {
+void *bubble_sort() 
+{
 	pthread_mutex_lock(&mutex);
 	int aux = 0;
-	for (int i = 0 ; i < BUFFER - 1; i++ ) {
-		for (int j = 0; j < BUFFER - 1; j++) {
-			if ( numeros[j] > numeros[j+1] ) {
+	for (int i = 0 ; i < BUFFER - 1; i++ ) 
+	{
+		for (int j = 0; j < BUFFER - 1; j++) 
+		{
+			if ( numeros[j] > numeros[j+1] ) 
+			{
 				aux = numeros[j];
 				numeros[j] = numeros[j+1];
 				numeros[j+1] = aux;
 			}
 		}
 	}
+	cont++;
 	sem_post(&sort_condition); // Emite um sinal para a thread mostrar_vetor() //
 	pthread_mutex_unlock(&mutex); // libera o recurso //
 	pthread_exit(0);
 }
 
-void *mostrar_vetor() {
-	pthread_mutex_trylock(&mutex); // Tenta o acesso ao recurso //
-	sem_wait(&sort_condition); // Espera o sinal da thread de ordenação //
-	printf("=== Vetor ordenado! ===\n");
-	for (int k = 0; k < BUFFER; k++)
-		printf("|%d|", numeros[k]);
-	printf("\n");
-	pthread_mutex_unlock(&mutex); // libera o recurso //
-	pthread_exit(0);
+void *mostrar_vetor() 
+{
+	while(1)
+	{
+		pthread_mutex_trylock(&mutex); // Tenta o acesso ao recurso //
+		// verifica a flag passando a vez em caso de não poder executar//	
+		if(cont == 0)
+		{
+			pthread_mutex_unlock(&mutex); // deixa a zona crítica.
+			sched_yield(); // passa a vez.
+		}
+		else
+		{
+			sem_wait(&sort_condition); // Espera o sinal da thread de ordenação //
+			printf("=== Vetor ordenado! ===\n");
+			for (int k = 0; k < BUFFER; k++)
+			{
+				printf("|%d|", numeros[k]);
+			}
+			printf("\n");
+			pthread_mutex_unlock(&mutex); // libera o recurso //
+			break;
+		}
+	}
+		pthread_exit(0);
 }
