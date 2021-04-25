bcba16bef899bb4801af16ad44410f385253ae42
Euronym <63007760+Euronym@users.noreply.github.com>
2021-04-25 20:43:25 -0300

    Update p4.c

--- 2_processos_e_threads/p4.c
+++ 2_processos_e_threads/p4.c
@@ -34,56 +34,54 @@ void* printB()
 	sem_wait(&condA); 
 	// requisita acesso à zona crítica.
 	pthread_mutex_lock(&o_mutex); 
-
+	// exibe a mensagem.
 	printf("B");
 	// sinaliza para o thread C.
 	sem_post(&condB); 
 	// libera o recurso.
 	pthread_mutex_unlock(&o_mutex); 
-	//	finaliza
+	// finaliza
 	pthread_exit(0);
 }
 
 void* printC()
 {
-
 	// aguarda a thread B executar.
 	sem_wait(&condB); 
 	// tenta utilizar o recurso.
 	pthread_mutex_lock(&o_mutex); 
+	// exibe a mensagem.
 	printf("C\n");
 	// sai da zona crítica
 	pthread_mutex_unlock(&o_mutex); 
 	//finaliza
 	pthread_exit(0);	
-
 }
 
 int main()
 {
-    //definindo as variáveis que armazenarão as ids das threads
+    	//definindo as variáveis que armazenarão as ids das threads
 	pthread_t tA, tB, tC;
 	// inicializa o mutex.
 	pthread_mutex_init(&o_mutex,0); 
 	// inicializa os semáforos
-    sem_init(&condA, 0, 0);
-    sem_init(&condB, 0, 0);
+    	sem_init(&condA, 0, 0);
+   	sem_init(&condB, 0, 0);
 	// cria as threads.
    	pthread_create(&tA,0,printA,0);
    	pthread_create(&tB,0,printB,0);
    	pthread_create(&tC,0,printC,0);
  	//espera execução da thread com id = tA (que imprime A)
-    pthread_join(tA, 0);
-    // espera a execução da thread com id = TB
+    	pthread_join(tA, 0);
+   	// espera a execução da thread com id = TB
 	pthread_join(tB, 0);
 	// espera a execução da thread com id = TC
 	pthread_join(tC, 0); 
 	// destroi o semáforo A.
 	sem_destroy(&condA);
 	// destroi o semáforo B. 
-    sem_destroy(&condB); 
-    // destroi o mutex.
-    pthread_mutex_destroy(&o_mutex); 
-    
-    return 0;
+    	sem_destroy(&condB); 
+   	// destroi o mutex.
+    	pthread_mutex_destroy(&o_mutex); 
+    	return 0;
 }
