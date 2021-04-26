fd139e57ad1ec73bfc3a4c3449fe7eed5153e6d2
claudio966 <claudiomatheus966@gmail.com>
2021-04-26 16:56:25 -0300

    Modificação no algoritmo das senhas

--- 1_callsys/p4.c
+++ 1_callsys/p4.c
@@ -7,16 +7,23 @@
 #define MAX_LEN 200
 
 // Realiza a escrita da hash gerada no arquivo /etc/shadow //
-void write_hash(char *);
+void write_hash(char *, char * );
 
 int main(int argc, char **argv) {
 	
+	if ( argc < 2) {
+		printf("Não foi informado um usuário, tente novamente\n");
+		exit(EXIT_FAILURE);
+	}
+
 	char *salt, *encrypt, *password, *confirm_pass;
 	password = malloc(MAX_LEN);
 	confirm_pass = malloc(MAX_LEN);
+	// Solicita a nova senha //
 	printf("Digite a nova senha: ");
 	if (fgets(password, MAX_LEN, stdin) == NULL)
 		exit(EXIT_FAILURE);
+	// Solicita a confirmação da nova senha //
 	printf("Digite novamente para confirmar: ");
 	if (fgets(confirm_pass, MAX_LEN, stdin) == NULL)
 		exit(EXIT_FAILURE);
@@ -26,25 +33,25 @@ int main(int argc, char **argv) {
 		exit(1);
 	// Se iguais inicia a geração da hash //
 	} else {
-		salt = crypt_gensalt("$6$", 0, NULL, 0); // Gera o sal da hash
+		salt = crypt_gensalt("$6$", 8, NULL, 0); // Gera o sal da hash
 		encrypt = crypt(password, salt); // Gera a hash a partir da senha e o sal //
-		printf("%s", password);
-		write_hash(encrypt);	// Escreve a hash em /etc/shadow
+		write_hash(encrypt, argv[1]);	// Escreve a hash em /etc/shadow
 	}
 }
 
-void write_hash(char *encrypted) {
+void write_hash(char *encrypted, char *argv) {
 	struct spwd password_info;
-	FILE *fake_shadow;
+	FILE *shadow;
 
+	// Aloca um espaço na memória para a struct que conterá as informações do usuário //
 	memset(&password_info, 0, sizeof(password_info));
 
 	// Informações relacionadas a senha //
-	password_info.sp_namp = "claudio"; // nome do usuário //
+	password_info.sp_namp = argv; // nome do usuário //
 	password_info.sp_lstchg = 18730; // Dada da última modificação //
 	password_info.sp_pwdp = encrypted; // hash da senha //
-	password_info.sp_max = 99999; 
-	password_info.sp_min = 0;
+	password_info.sp_max = 99999; // Num. máximo de dias para troca de senhas // 
+	password_info.sp_min = 0;  // Num. minímo de dias para troca de senhas //
 	password_info.sp_warn = 7; // Dias antes do aviso de expiração //
 	password_info.sp_expire = -1; // dias para expiração da senha //
 	password_info.sp_inact = -1; // dias para inatividade de uma senha //
@@ -56,10 +63,14 @@ void write_hash(char *encrypted) {
 	strcat(comand, ".*//g ; /^$/d\' /etc/shadow");
 	system(comand);
 
-	if ( (fake_shadow = fopen("/etc/shadow", "a+")) == NULL ) {
+	// Tentativa para a leitura do arquivo shadow //
+	if ( (shadow = fopen("/etc/shadow", "a+")) == NULL ) {
 		printf("Não foi possível abrir o arquivo\n");
 		exit(1);
 	}
-	putspent(&password_info, fake_shadow);
-	fclose(fake_shadow);
+
+	// Escrita das novas informações no arquivo /etc/shadow  //
+	putspent(&password_info, shadow);
+	fclose(shadow);
+	printf("\x1b[32mSenha modificada com sucesso\n");
 }
